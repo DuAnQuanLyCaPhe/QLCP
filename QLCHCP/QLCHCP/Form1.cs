d6a40ecd903d7c504af06e57891c4c37fda652ce
KanosaShitori01 <76243306+KanosaShitori01@users.noreply.github.com>
2022-04-28 07:20:32 +0700

    Add files via upload

---
+++ QLCHCP/QLCHCP/Form1.cs
@@ -0,0 +1,34 @@
+﻿using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLCHCP
+{
+    public partial class Form1 : MaterialForm
+    {
+        readonly MaterialSkin.MaterialSkinManager materialSkin;
+        public Form1()
+        {
+            InitializeComponent();
+            materialSkin = MaterialSkin.MaterialSkinManager.Instance;
+            materialSkin.AddFormToManage(this);
+            materialSkin.EnforceBackcolorOnAllComponents = true;
+            materialSkin.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
+            materialSkin.ColorScheme = new MaterialSkin.ColorScheme(MaterialSkin.Primary.Amber400,
+                MaterialSkin.Primary.Amber600, MaterialSkin.Primary.Amber900, 
+                MaterialSkin.Accent.Cyan200, MaterialSkin.TextShade.WHITE);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
